3e9e4a13fba80f88e66cec0bec35826dba8e05aa
AnirudhGadhar <137680838+AnirudhGadhar@users.noreply.github.com>
2024-06-26 17:17:29 +0000

    01basics_in_js

--- 01basics/comparsion.js
+++
@@ -1,9 +0,0 @@
-console.log("2" > 1 );  // string n interger
-console.log(2>"1")
-
-console.log(null > 0)
-console.log(null == 0)
-console.log(null >= 0)
-console.log(undefined == 0)
-
-console.log("2" === 2)  // compare value andf type
---
+++ 01basics/datatypes.js
@@ -0,0 +1,21 @@
+// primitive  7 types
+
+/* string
+number
+boolean
+null
+undefined
+symbol
+bigInt  */
+
+// non primitive 
+
+/*
+array
+objects
+functions
+*/
+
+
+
+console.log()
