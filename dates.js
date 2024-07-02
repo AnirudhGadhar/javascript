ed6b87db3e7d446eeea93b81999f94ddda49093a
AnirudhGadhar <137680838+AnirudhGadhar@users.noreply.github.com>
2024-07-02 16:35:43 +0000

    date_time

---
+++ dates.js
@@ -0,0 +1,41 @@
+let mydate = new Date()
+console.log(mydate);
+
+let mdate = new Date()
+console.log(mdate.toString());
+console.log(mdate.toLocaleString());
+console.log(typeof mdate);
+
+let mycreatedate = new Date(2023,0,24)
+console.log(mycreatedate.toDateString());
+console.log(mycreatedate.toString());
+console.log(mycreatedate.toLocaleString());
+
+
+// ************* timestamps **************
+
+let timestamp = Date.now()
+console.log(timestamp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
