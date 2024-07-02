f6ccd8587517f364f8e96dcae90276a212d1cea0
AnirudhGadhar <137680838+AnirudhGadhar@users.noreply.github.com>
2024-07-02 16:17:40 +0000

    maths_and_num

---
+++ basics_in_js/nums_maths.js
@@ -0,0 +1,27 @@
+
+
+console.log(Math.abs(-3));  // converts negative to postive number
+console.log(Math.min(3,4,7,-1));  // helps to find min value in array
+
+console.log(Math.max(3,10000,100)); // helps to find max value in array
+console.log(Math.pow(3,2)); // power function
+
+console.log(Math.round(3.566)); // rounds by deafault
+console.log(Math.floor(3.9)); // rounds to previous number
+console.log(Math.ceil(3.1));  // rounds to next number
+
+
+
+
+
+console.log(Math.random());  // gives a number and value always between 0 and 1 
+console.log(Math.random(5)*10 + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min + 1) + min))
+
+
+
