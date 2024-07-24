c90c5c1d1a11d33f896a865f6dbe2d2216ecd7a8
AnirudhGadhar <137680838+AnirudhGadhar@users.noreply.github.com>
2024-07-24 14:57:30 +0000

    arrays_in_js

---
+++ arrays/1array.js
@@ -0,0 +1,33 @@
+const arr=[1,2,3,4,5];
+console.log(arr);
+
+// array methods
+
+arr.push(8,9,10);
+console.log(arr);
+
+arr.pop()
+console.log(arr);
+
+arr.unshift(0)
+console.log(arr);
+
+// unshift is add to key item in 0th index.
+
+
+arr.shift()
+console.log(arr);
+
+// shift is used to remove the key item from 0th index
+
+console.log(arr.includes(9));
+console.log(arr.includes(19));
+
+const newarray =arr.join();
+
+console.log(arr);
+console.log(newarray);
+console.log(typeof newarray);
+
+
+
